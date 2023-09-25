f352572484760184dcf2d8e04703af4e00ad616d
Gervais <49298555+Gervais1705@users.noreply.github.com>
2023-09-25 02:18:46 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {browerRouter} from 'react-routrer-dom'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+
   <App/>
-    </BrowserRouter>
+    
 )
