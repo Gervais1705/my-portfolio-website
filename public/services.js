35fe4f1f568321b405af87fda24646a7c67055dc
Gervais <49298555+Gervais1705@users.noreply.github.com>
2023-09-27 22:11:22 -0700

    Update services.js

--- public/services.js
+++ public/services.js
@@ -1,10 +1,20 @@
+
+
+import ruler from '../src/assets/icons/ruler-pen.png';
+import code from '../src/assets/icons/code.png';
+import android from '../src/assets/icons/android.png';
+
+import git from '../src/assets/icons/git-repo.png';
+import javascript from '../src/assets/icons/javascript.png';
+import web from '../src/assets/icons/web-scraping.png';
+
 const services = [
-    {id: 1, name:'Responsive Design', description:'I design beautiful & responsive web with React', image:'../src/assets/icons/ruler-pen.png'},
-    {id:2, name:'WEB DEVELOPMENT', description:'I create beautiful iterfaces with simple HTML, CSS, & JavaScript and also frameworks like   ReactJS', image:'../src/assets/icons/code.png'},
-    {id:3, name:'MOBILE DEVELOPMENT', description:'I am an expert mobile developer. I have experience using Tailwind and React Native.', image:'../src/assets/icons/android.png'},
-    {id:4, name:'VERSION CONTROL', description:'I can use version control systems well, and Git & Mecurial are my go-to tool.', image:'../src/assets/icons/git-repo.png'},
-    {id:5, name:'NPM AND NODEJS', description:'I have core understanding of NPM. I can also develop general purpose applications with NodeJS', image:'../src/assets/icons/javascript.png'},
-    {id:6, name:'API', description:'I master back-end API that helps to interact with back-end server.', image:'../src/assets/icons/web-scraping.png'},
+    {id: 1, name:'Responsive Design', description:'I design beautiful & responsive web with React', image:ruler},
+    {id:2, name:'WEB DEVELOPMENT', description:'I create beautiful iterfaces with simple HTML, CSS, & JavaScript and also frameworks like   ReactJS', image:code},
+    {id:3, name:'MOBILE DEVELOPMENT', description:'I am an expert mobile developer. I have experience using Tailwind and React Native.', image:android},
+    {id:4, name:'VERSION CONTROL', description:'I can use version control systems well, and Git & Mecurial are my go-to tool.', image: git},
+    {id:5, name:'NPM AND NODEJS', description:'I have core understanding of NPM. I can also develop general purpose applications with NodeJS', image:javascript},
+    {id:6, name:'API', description:'I master back-end API that helps to interact with back-end server.', image:web},
 ]
 
 export default services;
